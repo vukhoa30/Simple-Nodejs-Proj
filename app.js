7bb41c10beff7a759538d80499720ded2fa1496d
vukhoa30 <vukhoa32@gmail.com>
2017-08-07 21:38:54 +0700

    them process.env.PORT

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.post("/", function(req, res) {
     })
 })
 
-var server = app.listen(8081, function() {
+var server = app.listen(process.env.PORT || 8081, function() {
     var host = server.address().address
     var port = server.address().port
     /*
